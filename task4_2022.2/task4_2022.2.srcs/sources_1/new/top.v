e6b6b021f95b8c603e2bb56a0bf14a1df4964809
Raninninn <raninninn-devel@protonmail.com>
2026-02-18 01:01:34 +0100

    fix interrupts for task 4
    Interrupts are fixed by preventing latch inference and debouncing
    buttons and switches properly.

--- task4_2022.2/task4_2022.2.srcs/sources_1/new/top.v
+++ task4_2022.2/task4_2022.2.srcs/sources_1/new/top.v
@@ -28,38 +28,39 @@ module top(
   input [3:0] correct_bits_count,
   input set,
   output reg [7:0] curr_seq = 0,
-  output reg new_seq_ready = 0,
-  output reg lock = 0,
+  output wire new_seq_ready,
+  output wire lock,
   input int_ack
   );
 
   reg [7:0] next_seq = 0;
   reg next_new_seq_ready = 0;
   reg next_lock = 0;
-  reg gone_to_locking_state = 0;
-  reg gone_to_switch_state = 0;
-  reg gone_reset = 0;
+  reg lock_reg = 0;
+  reg new_seq_ready_reg = 0;
+
+  assign lock = lock_reg;
+  assign new_seq_ready = new_seq_ready_reg;
 
   always @( posedge clk ) begin
     if ( !rst ) begin
-      new_seq_ready <= next_new_seq_ready;
-      lock <= next_lock;
+      new_seq_ready_reg <= next_new_seq_ready;
+      lock_reg <= next_lock;
       curr_seq <= next_seq; // {1'b1, int_ack, gone_reset, lock, /*next_lock,*/ gone_to_locking_state, gone_to_switch_state};
     end else begin
-      gone_reset <= 1;
-      {curr_seq, next_seq} <= 16'b0;
-      {new_seq_ready, next_new_seq_ready, lock, next_lock} <= 4'b0;
+      curr_seq <= 8'b0;
+      {new_seq_ready_reg, lock_reg} <= 2'b0;
     end
   end
 
-  always @( rst, set, input_bit_valid, int_ack ) begin
+  //always @( rst, set, input_bit_valid, int_ack ) begin
+  always @( posedge clk ) begin
     if ( !rst ) begin
       case ( {set, input_bit_valid, int_ack} )
         3'b010: begin
           next_seq[correct_bits_count] <= input_bit;
           next_new_seq_ready <= 1;
           next_lock <= 0;
-          gone_to_switch_state <= 1;
         end
         3'b001: begin
           next_lock <= 0;
@@ -68,13 +69,15 @@ module top(
         3'b100: begin
           next_lock <= 1;
           next_new_seq_ready <= 0;
-          gone_to_locking_state <= 1;
         end
         default: begin
           next_lock <= next_lock;
           next_new_seq_ready <= next_new_seq_ready;
         end
       endcase
+    end else begin
+      next_seq <= 8'b0;
+      {next_new_seq_ready, next_lock} <= 2'b0;
     end
   end
 endmodule
